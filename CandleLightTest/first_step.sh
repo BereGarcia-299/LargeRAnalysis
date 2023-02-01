9731721d1bc9970f549a9e3e15f871fa2b486088
Berenice Garcia <bereniceg299@spar0108.usatlas.bnl.gov>
2023-02-01 17:47:10 -0500

    This is my first step in order to remake RAA/jetrates comparisons between 2018 and 2015

---
+++ CandleLightTest/first_step.sh
@@ -0,0 +1,4 @@
+cp /usatlas/u/bereniceg299/data/LargeRJet_Study/NewSourceCode/LargeRAnalysis.C LargeRAnalysis/LargeRAnalysis.C #Analysis code
+cp /usatlas/u/bereniceg299/data/LargeRJet_Study/NewSourceCode/NewTreeVariables.h LargeRAnalysis/NewTreeVariables.C #Header File
+cp /usatlas/u/bereniceg299/data/LargeRJet_Study/NewSourceCode/SysUncert/sysUncert.C LargeRAnalysis/sysUncert.C #Calc Systematcis
+cp /usatlas/u/bereniceg299/data/LargeRJet_Study/NewSourceCode/SysUncert/UnfoldingJetsCopy.C LargeRAnalysis/sysUncert.C #Unfolding Code
